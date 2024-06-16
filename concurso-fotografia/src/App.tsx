ac7327d0667efd7f57a77e01ccc447c56d7f0c5e
Williamcom <william.com69@gmail.com>
2024-06-15 18:52:03 -0600

    elementos base

--- concurso-fotografia/src/App.tsx
+++ concurso-fotografia/src/App.tsx
@@ -1,4 +1,26 @@
-function App(){
-  return <p>Hola mundo</p>;
+import "./App.css"
+import Cabecera from './assets/Cabecera'
+import { Splitter, SplitterPanel } from 'primereact/splitter';
+
+export default function App(){
+
+  return (
+    <body>
+      <Cabecera />
+      <Splitter style={{ height: '300px' }}>
+          <SplitterPanel className="flex align-items-center justify-content-center">
+            <h3>
+            Ese vato que lastima la vista
+            </h3>
+            <h3>
+            Ese vato que lastima la vista
+            </h3>
+          </SplitterPanel>
+          <SplitterPanel className="flex align-items-center justify-content-center">
+            panel 2
+          </SplitterPanel>
+      </Splitter>
+
+    </body>
+  );
 }
-export default App;
