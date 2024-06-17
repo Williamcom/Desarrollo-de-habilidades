5d18f4785ee332a0c4dd8895e97d3913e2768d3d
Bruno-Reyes <garnelob@gmail.com>
2024-06-17 09:44:35 -0600

    Pagina de inicio a la medio hecha

--- concurso-fotografia/src/components/home/Home.tsx
+++ concurso-fotografia/src/components/home/Home.tsx
@@ -1,12 +1,56 @@
 import React from 'react';
+import { Card } from 'primereact/card';
+import { Galleria } from 'primereact/galleria';
 
-const Home: React.FC = () => {
+const HomePage = () => {
+    const photos = [
+        { src: '/Untitled.jpeg', alt: 'Photo 1' },
+        { src: 'path/to/photo2.jpg', alt: 'Photo 2' },
+        // Add more photos as needed
+    ];
+
+    const header = (
+        <div className="p-grid p-justify-center p-align-center">
+            <div className="p-col-12 p-md-8 p-lg-6">
+                <Card title="Concurso de Fotografía ESCOM 2024" subTitle="Organizado por el grupo 8BM1">
+                    <p className="p-mb-0">Participa en nuestro emocionante concurso de fotografía y muestra tu talento.</p>
+                </Card>
+            </div>
+        </div>
+    );
+
+    const rules = (
+        <div className="p-grid p-justify-center p-align-center p-mt-4">
+            <div className="p-col-12 p-md-10">
+                <Card title="Reglas del Concurso">
+                    <ul>
+                        <li>La participación es gratuita.</li>
+                        <li>Cada participante puede enviar fotos sin límite.</li>
+                        <li>Las fotos deben ser originales y no haber sido premiadas en otros concursos.</li>
+                        <li>La fecha límite para enviar las fotos es el 21 de Junio de 2024.</li>
+                    </ul>
+                </Card>
+            </div>
+        </div>
+    );
+
+    const gallery = (
+        <div className="p-grid p-justify-center p-align-center p-mt-4">
+            <div className="p-col-12 p-md-10">
+                <Card title="">
+                    <Galleria value={photos} showThumbnails showItemNavigators autoPlay transitionInterval={3000} />
+                </Card>
+            </div>
+        </div>
+    );
+
     return (
         <div>
-            <h2>Página de Inicio</h2>
-            <p>Contenido de la página de inicio...</p>
+            {header}
+            {rules}
+            {gallery}
         </div>
     );
 };
 
-export default Home;
+export default HomePage;
--- concurso-fotografia/src/components/statics/Loader.tsx
+++ concurso-fotografia/src/components/statics/Loader.tsx
@@ -1,8 +1,13 @@
 import React from 'react';
+import 'primereact/resources/themes/saga-blue/theme.css'; // Importa el tema de PrimeReact que prefieras
+import 'primereact/resources/primereact.min.css'; // Importa los estilos de PrimeReact
+
+import { ProgressSpinner } from 'primereact/progressspinner';
 
 const Loader: React.FC = () => {
     return (
         <div>
+            <ProgressSpinner/>
             <h2>Cargando . . .</h2>
         </div>
     );
